a4c47897474ca95fca6a394a8f36dc285e52f72d
Raoul Hopf <raoul.hopf@gmail.com>
2024-11-07 09:06:11 +0100

    v

--- indentation/indentationset.py
+++ indentation/indentationset.py
@@ -70,7 +70,7 @@ class IndentationSet:
         curves = []
         curve_dict = {
             "raw": {
-                "voltage": voltage, # FIX THIS LATER
+                "force": voltage, # FIX THIS LATER
                 "z": -z1,
                 "time": np.zeros(len(voltage)),
             },
